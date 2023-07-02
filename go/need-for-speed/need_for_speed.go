758c40be5707b6a56e9f471439a9b84eaca1c603
Nishant Mehta <nishme0708@gmail.com>
2023-07-02 21:43:11 +0530

    Update code for need for speed

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -3,24 +3,56 @@ package speed
 // TODO: define the 'Car' type struct
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+type Track struct {
+	distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
 }
 
 // TODO: define the 'Track' type struct
 
 // NewTrack creates a new track
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+	return Track{
+		distance: distance,
+	}
 }
 
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.batteryDrain > car.battery || car.battery == 0 {
+		return car
+	}
+	car.distance += car.speed
+	car.battery -= car.batteryDrain
+	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+
+	if car.batteryDrain > car.battery || car.battery == 0 {
+		return false
+	}
+	ticks := (track.distance - car.distance) / car.speed
+	car.batteryDrain = (ticks * car.batteryDrain)
+	if car.batteryDrain > car.battery {
+		return false
+	}
+	return true
 }
