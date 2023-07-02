828d28da72b20f88c2dcdac06323d00e2cbdd598
Nishant Mehta <nishme0708@gmail.com>
2023-07-02 11:23:36 +0530

    Add code for blackjack

---
+++ go/blackjack/blackjack.go
@@ -0,0 +1,62 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch card {
+        case "ace":
+    		return 11
+        case "two":
+    		return 2
+        case "three":
+    		return 3
+        case "four":
+    		return 4
+        case "five":
+    		return 5
+        case "six":
+    		return 6
+        case "seven":
+    		return 7
+        case "eight":
+    		return 8
+        case "nine":
+    		return 9
+        case "ten":
+    		fallthrough
+    	case "jack":
+    fallthrough
+    	case "queen":
+    fallthrough
+    	case "king":
+   			 return 10
+    default:
+    return 0   	
+    }
+return 0
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+    sum:=ParseCard(card1) + ParseCard(card2)
+    deal := ParseCard(dealerCard)
+    switch {
+        case card1=="ace" && card2=="ace":
+    		return "P"
+        case sum == 21 && deal<10:
+    		return "W"
+        case sum == 21 && deal>=10:
+    		return "S"
+        case sum >=17 && sum <=20:
+    		return "S"
+        case sum >=12 && sum<=16 && deal >=7:
+    		return "H"
+        case sum >=12 && sum<=16 :
+    		return "S"
+        case sum <=11:
+    		return "H"
+        default:
+    		return "S"
+    }
+return "S"
+}
